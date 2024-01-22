72ab3f0f127c95c6bceb22694cf4878247ccbb65
ibrahim cinar <cinar-ibrahim@outlook.com>
2024-01-22 12:59:46 +0300

    authorization resolved
    authorization resolved

--- newsAPI/src/main/java/com/cinar/newsAPI/model/Role.java
+++ newsAPI/src/main/java/com/cinar/newsAPI/model/Role.java
@@ -1,16 +1,17 @@
 package com.cinar.newsAPI.model;
 
 import org.springframework.security.core.GrantedAuthority;
-
 public enum Role implements GrantedAuthority {
     ROLE_USER("USER"),
     ROLE_ADMIN("ADMIN"),
     ROLE_MOD("MOD");
+
     private String value;
 
     Role(String value) {
         this.value = value;
     }
+
     public String getValue() {
         return this.value;
     }
--- newsAPI/src/main/java/com/cinar/newsAPI/security/SecurityConfig.java
+++ newsAPI/src/main/java/com/cinar/newsAPI/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.cinar.newsAPI.security;
 import com.cinar.newsAPI.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,19 +21,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
 
         security
-                .headers(x->x.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .headers(x -> x.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/swagger-ui/index.html").permitAll()
-                        .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/index.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/v1/api/news/news/{username}/**").hasRole(Role.ROLE_ADMIN.getValue())
+                        .requestMatchers(HttpMethod.POST, "/v1/api/user/**").hasRole(Role.ROLE_ADMIN.getValue())
+                        .requestMatchers("/v1/api/comment/**").hasRole(Role.ROLE_ADMIN.getValue())
                         .requestMatchers("/v1/api/user/**").permitAll()
-                        .requestMatchers("/api/v1/news/**").hasRole(Role.ROLE_ADMIN.getValue())
-
                         .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
         return security.build();
-
     }
 }
